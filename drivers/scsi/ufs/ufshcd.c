9b9b77fc47310d8610be70afbb8e0dc9cff8ee0d
fukiame <fukiame@proton.me>
2025-07-02 23:17:28 +0700

    Revert "[ALPS05228418] scsi: ufs: dump more log when command timeout"
    This reverts commit 756c9164af38b6d093ba10de93c29e9e8153663a.
    
    Signed-off-by: fukiame <fukiame@proton.me>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -6300,9 +6300,6 @@ static int ufshcd_abort(struct scsi_cmnd *cmd)
 	}
 
 	ufshcd_hold(hba, false);
-	dev_info(hba->dev,
-		"abort: tag %d, cmd 0x%x\n", tag, (int)cmd->cmnd[0]);
-
 	reg = ufshcd_readl(hba, REG_UTP_TRANSFER_REQ_DOOR_BELL);
 	/* If command is already aborted/completed, return SUCCESS */
 	if (!(test_bit(tag, &hba->outstanding_reqs))) {

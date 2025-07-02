5e38fe213338ac70078675aa591ef678f8c93e77
fukiame <fukiame@proton.me>
2025-07-02 18:33:11 +0700

    Revert "[ALPS04993096] sched_mon: modify macro definition"
    This reverts commit dd487c5cc1413e0aed207433b5d682bfc4cbd3c0.
    
    Signed-off-by: fukiame <fukiame@proton.me>

--- drivers/misc/mediatek/include/mt-plat/mtk_sched_mon.h
+++ drivers/misc/mediatek/include/mt-plat/mtk_sched_mon.h
@@ -64,7 +64,8 @@ DECLARE_PER_CPU(struct irq_handle_status, softirq_note);
 		time = sched_clock(); \
 } while (0)
 
-#define __check_process_time(msg, ts, ...) do { \
+#define __check_process_time(msg, _ts, ...) do { \
+	unsigned long long ts = _ts; \
 	unsigned long long te = sched_clock(); \
 	unsigned long long t_diff = te - ts; \
 					\
@@ -88,7 +89,9 @@ DECLARE_PER_CPU(struct irq_handle_status, softirq_note);
 		__check_process_time(msg, ts, ##__VA_ARGS__); \
 } while (0)
 
-#define check_preempt_count(count, msg, ...) do { \
+#define check_preempt_count(_count, msg, ...) do { \
+	int count = _count; \
+	\
 	if (count != preempt_count()) { \
 		char msg_str[128]; \
 				\
@@ -110,7 +113,8 @@ DECLARE_PER_CPU(struct irq_handle_status, softirq_note);
 	} \
 } while (0)
 
-#define __check_process_time_preempt(type, count, msg, ts, ...) do { \
+#define __check_process_time_preempt(type, count, msg, _ts, ...) do { \
+	unsigned long long ts = _ts; \
 	unsigned long long te = sched_clock(); \
 	unsigned long long t_diff = te - ts; \
 					\

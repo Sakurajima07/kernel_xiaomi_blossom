d0a581691d54a4984fbd1717b00f5ee0a376502d
fukiame <fukiame@proton.me>
2025-07-02 18:33:22 +0700

    Revert "[ALPS05204026] mtprof: fix converity scan"
    This reverts commit 11109fc74ac20884ad9447dffd5c36454836832c.
    
    Signed-off-by: fukiame <fukiame@proton.me>

--- drivers/misc/mediatek/mtprof/prof_main.c
+++ drivers/misc/mediatek/mtprof/prof_main.c
@@ -37,7 +37,6 @@
 		}						\
 	} while (0)
 
-#define SIGNAL_DELIVER_RES_LEN 5
 static const char * const signal_deliver_results[] = {
 	"delivered",
 	"ignored",
@@ -72,7 +71,7 @@ static void probe_signal_generate(void *ignore, int sig, struct siginfo *info,
 	 */
 	STORE_SIGINFO(errno, code, info);
 
-	if (result < 0 || result >= SIGNAL_DELIVER_RES_LEN)
+	if (result < 0 || result >= sizeof(signal_deliver_results))
 		res_state = false;
 
 	printk_deferred("[signal][%d:%s]generate sig %d to [%d:%s:%c] errno=%d code=%d grp=%d res=%s\n",
